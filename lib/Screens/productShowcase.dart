099a3ca179623505f8fda09a710000db5973dd4c
Yash Paneliya <yashsp1158@gmail.com>
2020-07-08 23:32:38 +0530

    Minor improvements

--- lib/Screens/productShowcase.dart
+++ lib/Screens/productShowcase.dart
@@ -1,5 +1,9 @@
+import 'package:astron_bags/Screens/adminHomePage.dart';
+import 'package:astron_bags/Screens/productDetails.dart';
+import 'package:astron_bags/models/productModel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:astron_bags/main.dart';
 
 QuerySnapshot qn, snapshot;
 
@@ -21,45 +25,94 @@ class _ProductsState extends State<Products> {
         .then((results) {
       snapshot = results;
     });
-    print(snapshot.documents.elementAt(0));
+    print(snapshot.documents.length);
     return snapshot.documents;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: astronColor,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          widget.category,
+          style: TextStyle(color: astronColor),
+        ),
+      ),
       body: SafeArea(
         child: FutureBuilder(
           future: getAllProducts(),
           builder: (_, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
+              return Center(
+                  child: CircularProgressIndicator(
+                backgroundColor: astronColor,
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ));
             } else {
+              if (snapshot.data.length==0) {
+                return Center(
+                  child: Text(
+                    'No data available!!!',
+                    style: TextStyle(fontSize: 25.0,color: Colors.black),
+                  ),
+                );
+              }
               return GridView.builder(
                   itemCount: snapshot.data.length,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisSpacing: 10.0,
+                      mainAxisSpacing: 10.0,
                       crossAxisCount: 2),
                   itemBuilder: (_, index) {
-                    return Card(
-                      elevation: 8.0,
-                      child: Container(
-                        child: Column(
-                          children: [
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.4,
-                              height: MediaQuery.of(context).size.height * 0.2,
-                              margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                              child: Image.network(
-                                  snapshot.data[index].data['imgLink']),
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductDetails(
+                              product: ProductModel(
+                                  snapshot.data[index].data['model'].toString(),
+                                  snapshot.data[index].data['imgLink'],
+                                  widget.category),
                             ),
-                            Text(
-                              snapshot.data[index].data['model']
-                                  .toString()
-                                  .toUpperCase(),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 20.0),
-                            )
-                          ],
+                          ),
+                        );
+                      },
+                      child: Card(
+                        elevation: 8.0,
+                        child: Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.4,
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                margin:
+                                    EdgeInsets.only(top: 10.0, bottom: 10.0),
+                                child: Image.network(
+                                    snapshot.data[index].data['imgLink']),
+                              ),
+                              Text(
+                                snapshot.data[index].data['model']
+                                    .toString()
+                                    .toUpperCase(),
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     );
